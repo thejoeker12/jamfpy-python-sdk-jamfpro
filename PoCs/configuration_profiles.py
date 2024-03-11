90dde688230745da91a5c1ccaea6c711e2ff00c7
Joseph Little <joelittle11@live.com>
2024-03-11 16:18:19 +0000

    more debug usage

--- PoCs/configuration_profiles.py
+++ PoCs/configuration_profiles.py
@@ -27,54 +27,54 @@ client = jamfpi.init_client(
     logging_level=logging.WARN
 )
 
-def test_in_out_key():
-    RANDOM_NUMBER = random.randint(10, 10000)
-    NEW_PROFILE_NAME = f"Test Profile Nothing {RANDOM_NUMBER}"
-    print(f"Sending All Computers as: {False}")
+# def test_in_out_key():
+#     RANDOM_NUMBER = random.randint(10, 10000)
+#     NEW_PROFILE_NAME = f"Test Profile Nothing {RANDOM_NUMBER}"
+#     print(f"Sending All Computers as: {False}")
 
-    # Create New
-    new_profile_payload = open("payload.mobileconfig", "r").read().format(NAME=NEW_PROFILE_NAME, ALL_COMPUTERS=False)
-    print(new_profile_payload)
-    create_new_profile = client.classic.configuration_profiles.create(new_profile_payload, "json")
+#     # Create New
+#     new_profile_payload = open("payload.mobileconfig", "r").read().format(NAME=NEW_PROFILE_NAME, ALL_COMPUTERS=False)
+#     print(new_profile_payload)
+#     create_new_profile = client.classic.configuration_profiles.create(new_profile_payload, "json")
 
-    # Get ID from creation
-    if create_new_profile.ok:
-        root = ElementTree.fromstring(create_new_profile.text)
-        new_profile_id = root.find('id').text
-        print(f"Created new profile with id: {new_profile_id}")
-    else:
-        raise Exception("Failed to make profile")
+#     # Get ID from creation
+#     if create_new_profile.ok:
+#         root = ElementTree.fromstring(create_new_profile.text)
+#         new_profile_id = root.find('id').text
+#         print(f"Created new profile with id: {new_profile_id}")
+#     else:
+#         raise Exception("Failed to make profile")
     
-    # Get profile and check all_computers
-    get_profile = client.classic.configuration_profiles.get_by_id(new_profile_id, "json")
-    get_profile_json = get_profile.json()
-    all_computers = get_profile_json["os_x_configuration_profile"]["scope"]["all_computers"]
-    print(f"Received All Computers as: {all_computers}")
+#     # Get profile and check all_computers
+#     get_profile = client.classic.configuration_profiles.get_by_id(new_profile_id, "json")
+#     get_profile_json = get_profile.json()
+#     all_computers = get_profile_json["os_x_configuration_profile"]["scope"]["all_computers"]
+#     print(f"Received All Computers as: {all_computers}")
 
-    # Send update to flip the bool
-    print("Switching")
-    new_profile_payload = open("payload.mobileconfig", "r").read().format(NAME=NEW_PROFILE_NAME, ALL_COMPUTERS=True)
-    print(new_profile_payload)
-    update_switch = client.classic.configuration_profiles.update_by_id(new_profile_id, new_profile_payload)
+#     # Send update to flip the bool
+#     print("Switching")
+#     new_profile_payload = open("payload.mobileconfig", "r").read().format(NAME=NEW_PROFILE_NAME, ALL_COMPUTERS=True)
+#     print(new_profile_payload)
+#     update_switch = client.classic.configuration_profiles.update_by_id(new_profile_id, new_profile_payload)
 
-    # Check update ok
-    if update_switch.ok:
-        print("Switched successfully")
-        get_profile = client.classic.configuration_profiles.get_by_id(new_profile_id, "json")
-        get_profile_json = get_profile.json()
-        all_computers = get_profile_json["os_x_configuration_profile"]["scope"]["all_computers"]
-        print(f"Swtiched All Computers as: {all_computers}")
+#     # Check update ok
+#     if update_switch.ok:
+#         print("Switched successfully")
+#         get_profile = client.classic.configuration_profiles.get_by_id(new_profile_id, "json")
+#         get_profile_json = get_profile.json()
+#         all_computers = get_profile_json["os_x_configuration_profile"]["scope"]["all_computers"]
+#         print(f"Swtiched All Computers as: {all_computers}")
 
     
 
-    # Cleanup
+#     # Cleanup
 
-    print("Cleaning up...")
-    delete_profile = client.classic.configuration_profiles.delete_by_id(new_profile_id)
-    if delete_profile.ok:
-        print("Deleted profile successfully")
-    else: 
-        raise Exception("Failed to delete profile")
+#     print("Cleaning up...")
+#     delete_profile = client.classic.configuration_profiles.delete_by_id(new_profile_id)
+#     if delete_profile.ok:
+#         print("Deleted profile successfully")
+#     else: 
+#         raise Exception("Failed to delete profile")
     
 
 
@@ -95,17 +95,17 @@ def cleanup():
             raise Exception(f"Failed to delete profile {profile_id}")
 
 
-def test_get():
-    prof_id = 386
-    get = client.classic.configuration_profiles.get_by_id(prof_id)
-    dom = xml.dom.minidom.parseString(get.text)
-    pretty = dom.toprettyxml(indent="   ")
-    pretty = "\n".join(line for line in pretty.split("\n") if line.strip())
-    print(pretty)
+# def test_get():
+#     prof_id = 393
+#     get = client.classic.configuration_profiles.get_by_id(prof_id)
+#     dom = xml.dom.minidom.parseString(get.text)
+#     pretty = dom.toprettyxml(indent="   ")
+#     pretty = "\n".join(line for line in pretty.split("\n") if line.strip())
+#     print(pretty)
 
 
 def main():
-    test_get()
+    cleanup()
 
 
 main()
