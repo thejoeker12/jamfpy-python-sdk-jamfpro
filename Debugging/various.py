787d7a2f30f0b3376e158821c0b66891e534ae4f
Joseph Little <joelittle11@live.com>
2024-06-24 16:20:14 +0100

    debugging

--- Debugging/various.py
+++ Debugging/various.py
@@ -13,6 +13,7 @@ sys.path.append(parent_dir)
 import jamfpi
 import requests
 from xml.dom.minidom import parseString
+from pprint import pprint
 
 config = jamfpi.import_json("clientauth.json")
 
@@ -22,11 +23,32 @@ jamf = jamfpi.init_client(
     client_secret=config["clientSecret"]
 )
 
-request = requests.Request("GET", url="https://lbgsandbox.jamfcloud.com/JSSResource/accounts/userid/505")
+request = requests.Request("GET", url="https://lbgsandbox.jamfcloud.com/api/v1/scripts")
 resp = jamf.classic.do(request, error_on_fail=False)
-# print(resp.text)
+if resp.ok:
+    rjson = resp.json()["results"]
+    for i in rjson:
+        request = requests.Request("DELETE", url=f"https://lbgsandbox.jamfcloud.com/api/v1/scripts/{i["id"]}")
+        resp = jamf.classic.do(request, error_on_fail=False)
 
 
-xml_dom = parseString(resp.text)
-pretty_xml_as_string = xml_dom.toprettyxml()
-print(pretty_xml_as_string)
+data = {
+    "name": "script name",
+    # "categoryId": 5
+}
+
+request = requests.Request("POST", url="https://lbgsandbox.jamfcloud.com/api/v1/scripts", json=data)
+resp = jamf.classic.do(request, error_on_fail=False)
+if resp.ok:
+    pprint(resp.json())
+    rjson = resp.json()
+else:
+    print("error")
+
+
+request = requests.Request("GET", url=f"https://lbgsandbox.jamfcloud.com/api/v1/scripts/{rjson["id"]}")
+resp = jamf.classic.do(request, error_on_fail=False)
+if resp.ok:
+    pprint(resp.json())
+else:
+    print("error")
