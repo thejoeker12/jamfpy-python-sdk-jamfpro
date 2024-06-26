fea48dc5292229f724abf72cce3f4537c4c5bdab
thejoeker12 <joelittle11@live.com>
2024-06-26 15:08:13 +0100

    debugging

--- Debugging/various.py
+++ Debugging/various.py
@@ -23,32 +23,39 @@ jamf = jamfpi.init_client(
     client_secret=config["clientSecret"]
 )
 
-request = requests.Request("GET", url="https://lbgsandbox.jamfcloud.com/api/v1/scripts")
+
+request = requests.Request("GET", url="https://lbgsandbox.jamfcloud.com/JSSResource/computerextensionattributes/id/5290")
 resp = jamf.classic.do(request, error_on_fail=False)
-if resp.ok:
-    rjson = resp.json()["results"]
-    for i in rjson:
-        request = requests.Request("DELETE", url=f"https://lbgsandbox.jamfcloud.com/api/v1/scripts/{i["id"]}")
-        resp = jamf.classic.do(request, error_on_fail=False)
+print(resp.text)
+xml = parseString(resp.text)
+print(xml.toprettyxml())
+
+# request = requests.Request("GET", url="https://lbgsandbox.jamfcloud.com/api/v1/scripts")
+# resp = jamf.classic.do(request, error_on_fail=False)
+# if resp.ok:
+#     rjson = resp.json()["results"]
+#     for i in rjson:
+#         request = requests.Request("DELETE", url=f"https://lbgsandbox.jamfcloud.com/api/v1/scripts/{i['id']}")
+#         resp = jamf.classic.do(request, error_on_fail=False)
 
 
-data = {
-    "name": "script name",
-    # "categoryId": 5
-}
+# data = {
+#     "name": "script name",
+#     # "categoryId": 5
+# }
 
-request = requests.Request("POST", url="https://lbgsandbox.jamfcloud.com/api/v1/scripts", json=data)
-resp = jamf.classic.do(request, error_on_fail=False)
-if resp.ok:
-    pprint(resp.json())
-    rjson = resp.json()
-else:
-    print("error")
+# request = requests.Request("POST", url="https://lbgsandbox.jamfcloud.com/api/v1/scripts", json=data)
+# resp = jamf.classic.do(request, error_on_fail=False)
+# if resp.ok:
+#     pprint(resp.json())
+#     rjson = resp.json()
+# else:
+#     print("error")
 
 
-request = requests.Request("GET", url=f"https://lbgsandbox.jamfcloud.com/api/v1/scripts/{rjson["id"]}")
-resp = jamf.classic.do(request, error_on_fail=False)
-if resp.ok:
-    pprint(resp.json())
-else:
-    print("error")
+# request = requests.Request("GET", url=f"https://lbgsandbox.jamfcloud.com/api/v1/scripts/{rjson['id']}")
+# resp = jamf.classic.do(request, error_on_fail=False)
+# if resp.ok:
+#     pprint(resp.json())
+# else:
+#     print("error")
