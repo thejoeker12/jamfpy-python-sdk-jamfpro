9ac89aee8407eda60fa70c804d81058695909ae9
Joseph Little <joelittle11@live.com>
2024-08-13 11:14:45 +0100

    ignore and readme changes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-test
+An Object Oriented SDK for Jamf Pro written in Python
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='jamfpy',
-    version='1.0.0',
+    version='0.1.0',
     packages=find_packages()
 )
