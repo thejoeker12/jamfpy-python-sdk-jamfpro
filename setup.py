7108bc2171992370e60982db1d9682b809dd2aa5
Joseph Little <joelittle11@live.com>
2024-08-14 12:28:38 +0100

    fixing packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='jamfpy',
-    version='0.1.0',
-    packages=find_packages()
+    version='0.1.1',
+    packages=["jamfpy"]
 )
