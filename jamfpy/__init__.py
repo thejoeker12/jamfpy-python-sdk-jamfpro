21b7802715d31547f78e742548bf02234d3de0b0
Joseph Little <joelittle11@live.com>
2024-08-14 12:38:26 +0100

    another tweak

--- jamfpy/__init__.py
+++ jamfpy/__init__.py
@@ -1,19 +1,22 @@
 """Master init for jamfpy Library"""
 
-# from .client.init_client import init_client
-# from .client.utility import (
-#     import_json,
-#     get_bearer_token,
-#     generate_client_token,
-#     fix_jamf_time_to_iso,
-#     pretty_xml
-# )
+from .client.init_client import init_client
+from .client.utility import (
+    import_json,
+    get_bearer_token,
+    generate_client_token,
+    fix_jamf_time_to_iso,
+    pretty_xml
+)
 
-# from .client.utility import (
-#     compare_dict_keys,
-#     format_jamf_datetime,
-# )
+from .client.utility import (
+    compare_dict_keys,
+    format_jamf_datetime,
+)
 
-# from .client.client import (
-#     JamfTenant
-# )
+from .client.client import (
+    JamfTenant
+)
+
+
+__all__ = [init_client, JamfTenant]
