cea9e30ee9f12de9da8f3491c289520cc9f94c0e
Joseph <joelittle11@live.com>
2025-02-10 15:24:28 +0000

    more endpoints added

---
+++ jamfpy/endpoints/clc_advanced_computer_searches.py
@@ -0,0 +1,18 @@
+"""computer advanced searches"""
+
+from requests import Request
+from ._parent import Endpoint
+from ..client.client import API
+
+class AdvancedComputerSearches(Endpoint):
+    _uri = "/advancedcomputersearches"
+
+    def get_all(self):
+        suffix = self._uri
+        return self._api.do(
+            Request(
+                "GET",
+                url = self._api.url() + suffix,
+                headers = self._api.header("read")["json"]
+            )
+        )
--- jamfpy/endpoints/clc_computer_extension_attributes.py
+++ jamfpy/endpoints/clc_computer_extension_attributes.py
@@ -4,16 +4,27 @@ from requests import Request, Response
 from ._parent import Endpoint
 
 class ExtensionAttributes(Endpoint):
-    _uri = "/computerextensionattributes/id/0"
+    _uri = "/computerextensionattributes"
 
-    def create(self, xml: str) -> Response:
+    def get_all(self) -> Response:
         suffix = self._uri
+        return self._api.do(
+            Request(
+                "GET",
+                url = self.api.url() + suffix,
+                headers=self._api.header("read")["json"]
+            )
+        )
+
+
+    def create(self, payload_xml: str) -> Response:
+        suffix = self._uri + "/id/0"
         return self._api.do(
             Request(
                 method="POST",
                 url=self._api.url() + suffix,
                 headers=self._api.header("create-update")["xml"],
-                data=xml
+                data=payload_xml
             )
         )
 
