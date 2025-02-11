2cd125c8a9cffb766fdea5aefe1d46e972384510
Joseph <joelittle11@live.com>
2025-02-11 11:05:59 +0000

    removed another thing

--- jamfpy/endpoints/clc_categories.py
+++ jamfpy/endpoints/clc_categories.py
@@ -1,7 +1,6 @@
 """Jamf Classic API Endpoint Code for X"""
 from requests import Request, Response
 from ._parent import Endpoint
-from ..client.client import API
 
 class Categories(Endpoint):
     _uri = "/categories"
