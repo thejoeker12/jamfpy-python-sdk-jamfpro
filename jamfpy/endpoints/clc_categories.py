419a763f89fc7c7cc78747f3062580a2b2f480d7
Joseph <joelittle11@live.com>
2025-03-20 13:56:19 +0000

    added cat get by name

--- jamfpy/endpoints/clc_categories.py
+++ jamfpy/endpoints/clc_categories.py
@@ -25,6 +25,16 @@ class Categories(Endpoint):
                 headers = self._api.header("read")["json"]
             )
         )
+    
+    def get_by_name(self, target_name: str):
+        suffix = self._uri + f"/name/{target_name}"
+        return self._api.do(
+            Request(
+                method = "GET",
+                url=self._api.url() + suffix,
+                headers = self._api.header("read")["json"]
+            )
+        )
 
 
     def create(self, payload_xml):
