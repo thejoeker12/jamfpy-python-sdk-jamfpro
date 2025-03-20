45aa362864ed7d6271491d7fb1ceb9493be7b5f1
Joseph <joelittle11@live.com>
2025-03-20 15:20:18 +0000

    bob's destroying rowns

---
+++ jamfpy/endpoints/clc_buildings.py
@@ -0,0 +1,27 @@
+"""Jamf Classic API Endpoint for Buildings"""
+
+from requests import Request, Response
+from ._parent import Endpoint
+
+class Buildings(Endpoint):
+    _uri = "/buildings"
+
+    def get_all(self) -> Response:
+        suffix = self._uri
+        return self._api.do(
+            Request(
+                method = "GET",
+                url = self._api().url() + suffix,
+                headers = self._api.header("read")["json"]
+            )
+        )
+    
+    def delete_by_id(self, target_id) -> Response:
+        suffix = self._uri + f"/id/{target_id}"
+        return self._api.do(
+            Request(
+                method = "DELETE",
+                url=self._api.url()+ suffix,
+                headers = self._api.header("delete")["xml"]
+            )
+        )
