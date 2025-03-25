8d705b57860618386604647bd49122ec0025172c
Joseph <joelittle11@live.com>
2025-03-25 15:28:51 +0000

    computer group get by name

--- jamfpy/endpoints/clc_computer_groups.py
+++ jamfpy/endpoints/clc_computer_groups.py
@@ -26,6 +26,16 @@ class ComputerGroups(Endpoint):
                 headers=self._api.header("read")["json"]
             )
         )
+    
+    def get_by_name(self, target_id: int) -> Response:
+        suffix = self._uri + f"/name/{target_id}"
+        return self._api.do(
+            Request(
+                method="GET",
+                url=self._api.url() + suffix,
+                headers=self._api.header("read")["json"]
+            )
+        )
 
     def delete_by_id(self, target_id) -> Response:
         suffix = self._uri + f"/id/{target_id}"
