6cd21822b13916efd326172b5600d53f27b21853
Bobby Williams <42735259+bobbyjameswilliams@users.noreply.github.com>
2025-05-27 14:58:26 +0100

    Revert "conversion (#21)"
    This reverts commit a7c43c5a4da4e907fdb515f74602e56ae96913ef.

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Setup configuration for the jamfpy package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='jamfpy',
+    version='2.2.0-rc',
+    packages=find_packages()
+)
