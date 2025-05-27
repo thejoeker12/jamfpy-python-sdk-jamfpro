1e287de081e228c46c850c9183e807fe0cb704e5
Bobby <bobby.james.williams@outlook.com>
2025-05-27 14:21:03 +0100

    conversion

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""Setup configuration for the jamfpy package."""
-
-from setuptools import setup, find_packages
-
-setup(
-    name='jamfpy',
-    version='2.1.0-rc',
-    packages=find_packages()
-)
