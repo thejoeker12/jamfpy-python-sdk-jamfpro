658eb4df196a4d8e13811a2a007f2435a72b86f5
Joseph <joelittle11@live.com>
2025-09-08 09:09:29 +0100

    chore: restructured http config

--- jamfpy/client/constants.py
+++ jamfpy/client/constants.py
@@ -9,3 +9,60 @@ AUTH_REQUEST_TIMEOUT = 20
 TIME_ROUNDING_AMOUNT = 3
 
 VALID_AUTH_METHODS = ("oauth2", "basic")
+
+DEFAULT_HTTP_CONFIG_URLS = {
+    "base_cloud": "https://{tenant}.jamfcloud.com",
+    "auth": {
+        "bearer": "/api/v1/auth/token",
+        "oauth": "/api/oauth/token",
+        "invalidate-token": "/api/v1/auth/invalidate-token",
+        "keep-alive": "/api/v1/auth/keep-alive"
+    },
+    "api": {
+        "classic": "/JSSResource",
+        "pro": "/api/v{jamfapiversion}"
+    }
+}
+
+DEFAULT_HTTP_CONFIG_HEADERS = {
+        "auth": {
+            "oauth": {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "bearer": {
+                "accept": "application/json",
+                "Authorization": "Bearer {token}"
+            }
+        },
+        "crud": {
+            "create-update": {
+                "xml": {
+                    "accept": "text/xml",
+                    "content-type": "text/xml"
+                },
+                "json": {
+                    "accept": "application/json",
+                    "content-type": "application/json"
+                }
+            },
+            "read": {
+                "xml": {
+                    "accept": "text/xml"
+                },
+                "json": {
+                    "accept": "application/json"
+                }
+            },
+            "delete": {
+                "xml": {
+                    "accept": "text/xml"
+                },
+                "json": {
+                    "accept": "application/json"
+                }
+            },
+            "image": {
+                "accept": "image/*"
+            }
+        }
+    }
--- jamfpy/client/http_config.py
+++ jamfpy/client/http_config.py
@@ -1,68 +1,14 @@
 """HTTP configuration module for managing API request settings and configurations."""
 
+from .constants import DEFAULT_HTTP_CONFIG_URLS, DEFAULT_HTTP_CONFIG_HEADERS
+
 class HTTPConfig:
     """Configuration class for managing HTTP request settings, URLs, and headers for Jamf Pro API interactions."""
-    urls = {
-        "base_cloud": "https://{tenant}.jamfcloud.com",
-        "auth": {
-            "bearer": "/api/v1/auth/token",
-            "oauth": "/api/oauth/token",
-            "invalidate-token": "/api/v1/auth/invalidate-token",
-            "keep-alive": "/api/v1/auth/keep-alive"
-        },
-        "api": {
-            "classic": "/JSSResource",
-            "pro": "/api/v{jamfapiversion}"
-        }
-    }
-
-    headers = {
-        "auth": {
-            "oauth": {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            "bearer": {
-                "accept": "application/json",
-                "Authorization": "Bearer {token}"
-            }
-        },
-        "crud": {
-            "create-update": {
-                "xml": {
-                    "accept": "text/xml",
-                    "content-type": "text/xml"
-                },
-                "json": {
-                    "accept": "application/json",
-                    "content-type": "application/json"
-                }
-            },
-            "read": {
-                "xml": {
-                    "accept": "text/xml"
-                },
-                "json": {
-                    "accept": "application/json"
-                }
-            },
-            "delete": {
-                "xml": {
-                    "accept": "text/xml"
-                },
-                "json": {
-                    "accept": "application/json"
-                }
-            },
-            "image": {
-                "accept": "image/*"
-            }
-        }
-    }
 
     def __init__(
             self,
             urls: dict = None,
             headers: dict = None
     ):
-        self.urls = urls if urls is not None else self.urls
-        self.headers = headers if headers is not None else self.headers
+        self.urls = urls or DEFAULT_HTTP_CONFIG_URLS
+        self.headers = headers or DEFAULT_HTTP_CONFIG_HEADERS
