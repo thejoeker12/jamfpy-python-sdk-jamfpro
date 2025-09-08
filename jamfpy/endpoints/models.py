2984bcb10f5e7d76963737c8a244d202c3c55ece
Joseph <joelittle11@live.com>
2025-09-08 09:53:03 +0100

    feat: added back xml response optionally

--- jamfpy/endpoints/models.py
+++ jamfpy/endpoints/models.py
@@ -15,29 +15,32 @@ class Endpoint:
 class ClassicEndpoint(Endpoint):
     """Base class for Classic Jamf Pro API endpoints, implementing standard CRUD operations."""
 
-    def get_all(self, suffix: str | None = None) -> Response:
+    def get_all(self, suffix: str | None = None, xml_response: bool = False) -> Response:
         """Get all records for this endpoint.
         Optionally uses a provided suffix, otherwise defaults to the endpoint's base URI.
         """
         effective_suffix = suffix or self._uri
 
+        read_header = "json" if not xml_response else "xml"
+
         return self._api.do(
             Request(
                 method="GET",
                 url=self._api.url() + effective_suffix,
-                headers=self._api.header("read")["json"]
+                headers=self._api.header("read")[read_header]
             )
         )
 
 
-    def get_by_id(self, target_id: int) -> Response:
+    def get_by_id(self, target_id: int, xml_response: bool = False) -> Response:
         """Get a single record by ID."""
         suffix = self._uri + f"/id/{target_id}"
+        read_header = "json" if not xml_response else "xml"
         return self._api.do(
             Request(
                 method="GET",
                 url=self._api.url() + suffix,
-                headers=self._api.header("read")["json"]
+                headers=self._api.header("read")[read_header]
             )
         )
 
